86c4012d46a3912c193c69593a3755e45d96f72c
João Góes <joao_jdgf@hotmail.com>
2023-11-28 22:11:51 -0300

    mend

--- src/main/java/com/unicar/util/log/Logger.java
+++ src/main/java/com/unicar/util/log/Logger.java
@@ -2,7 +2,7 @@ package com.unicar.util.log;
 
 public class Logger {
     public static void log(String message) {
-        System.out.println("[UNICAR][LOG]: " message);
+        System.out.println("[UNICAR][LOG]: " + message);
     }
 
     public static void error(String message) {
